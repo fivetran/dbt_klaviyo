a2a4e900330e74e85339816977a023c6bbc0290c
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-04-21 15:44:11 -0700

    attribution var

--- models/intermediate/int_klaviyo__event_attribution.sql
+++ models/intermediate/int_klaviyo__event_attribution.sql
@@ -1,10 +1,7 @@
--- config as incremental....? will be a lil difficult
+-- config as incremental....? will need to grab all events related to a person (like mixpanel__sessions)
 with events as (
 
-    select
-        *
-        {# lag(occurred_at) over(partition by person_id order by occurred_at asc) as last_event_occurred_at #}
-
+    select *
     from {{ var('event_table') }}
 
     {# {% if is_incremental() %}
@@ -15,7 +12,6 @@ with events as (
 create_partitions as (
     select
         *,
-        {# {{ dbt_utils.datediff('last_event_occurred_at', 'occurred_at', 'minute') }} as  #}
         sum(case when campaign_id is null then 0 else 1 end) over (partition by person_id order by occurred_at) as campaign_partition,
         sum(case when flow_id is null then 0 else 1 end) over (partition by person_id order by occurred_at) as flow_partition
 
@@ -31,23 +27,27 @@ last_touches as (
         min(occurred_at) over(partition by person_id, flow_partition) as last_partition_touch_at
 
     from create_partitions
-        {# case when time_passed <= lookback_window then first_value
-        else null end as last_attributed_to_campaign_id, #}
 ),
 
 attribute as (
 
     select 
         *,
+
+        -- making default lookback = 3 hours
         coalesce(campaign_id,
             case 
             when {{ dbt_utils.datediff('last_campaign_touch_at', 'occurred_at', 'minute') }} <= {{ var('klaviyo__attribution_lookback_window', 180) }} 
+                and {{ var('klaviyo__event_attribution_filter', 'true') }}
+
                 then first_value(campaign_id) over (partition by person_id, campaign_partition order by occurred_at asc rows between unbounded preceding and current row)
-                else null end) last_touch_campaign_id,
+            else null end) last_touch_campaign_id,
 
         coalesce(flow_id,
             case 
             when {{ dbt_utils.datediff('last_campaign_touch_at', 'occurred_at', 'minute') }} <= {{ var('klaviyo__attribution_lookback_window', 180) }} 
+                and {{ var('klaviyo__event_attribution_filter', 'true') }}
+                
                 then first_value(flow_id) over (partition by person_id, flow_partition order by occurred_at asc rows between unbounded preceding and current row)
                 else null end) as last_touch_flow_id
 
