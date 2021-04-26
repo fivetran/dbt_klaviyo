2e2a6f47cea453511cfcc124dda037afbb4e5b15
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-04-26 15:11:06 -0700

    event attribution fixes

--- models/intermediate/int_klaviyo__event_attribution.sql
+++ models/intermediate/int_klaviyo__event_attribution.sql
@@ -12,8 +12,12 @@ with events as (
 create_partitions as (
     select
         *,
-        sum(case when campaign_id is null then 0 else 1 end) over (partition by person_id order by occurred_at) as campaign_partition,
-        sum(case when flow_id is null then 0 else 1 end) over (partition by person_id order by occurred_at) as flow_partition
+
+        -- default klaviyo__event_attribution_filter limits to email opens, email clicks, and sms opens
+        sum(case when campaign_id is not null and {{ var('klaviyo__event_attribution_filter') }} then 1 else 0 end) 
+            over (partition by person_id order by occurred_at) as campaign_partition,
+        sum(case when flow_id is not null and {{ var('klaviyo__event_attribution_filter') }} then 1 else 0 end) 
+            over (partition by person_id order by occurred_at) as flow_partition
 
     from events
 
@@ -25,7 +29,13 @@ last_touches as (
         *,
         -- the first event of each partition is the one with the non-null campaign/flow
         min(occurred_at) over(partition by person_id, campaign_partition) as last_campaign_touch_at,
-        min(occurred_at) over(partition by person_id, flow_partition) as last_flow_touch_at
+        min(occurred_at) over(partition by person_id, flow_partition) as last_flow_touch_at,
+
+        first_value(type) over(
+            partition by person_id, campaign_partition order by occurred_at asc rows between unbounded preceding and current row) as last_campaign_touch_event_type,
+
+        first_value(type) over(
+            partition by person_id, flow_partition order by occurred_at asc rows between unbounded preceding and current row) as last_flow_touch_event_type
 
     from create_partitions
 ),
@@ -35,22 +45,25 @@ attribute as (
     select 
         *,
 
-        -- making default lookback = 3 hours
+        -- making default lookback = 5 days (120 hours) for email and 1 day (24 hours) for sms
         coalesce(campaign_id,
             case 
-            when {{ dbt_utils.datediff('last_campaign_touch_at', 'occurred_at', 'minute') }} <= {{ var('klaviyo__attribution_lookback_window', 180) }} 
-                and {{ var('klaviyo__event_attribution_filter', 'true') }}
-
-                then first_value(campaign_id) over (partition by person_id, campaign_partition order by occurred_at asc rows between unbounded preceding and current row)
+            when {{ dbt_utils.datediff('last_campaign_touch_at', 'occurred_at', 'day') }} <= (
+                case when lower(last_campaign_touch_event_type) like '%sms%' then {{ var('klaviyo__sms_attribution_lookback') }}
+                    else {{ var('klaviyo__email_attribution_lookback') }} end)
+                then first_value(campaign_id) over (
+                    partition by person_id, campaign_partition order by occurred_at asc rows between unbounded preceding and current row)
             else null end) last_touch_campaign_id,
 
         coalesce(flow_id,
             case 
-            when {{ dbt_utils.datediff('last_flow_touch_at', 'occurred_at', 'minute') }} <= {{ var('klaviyo__attribution_lookback_window', 180) }} 
-                and {{ var('klaviyo__event_attribution_filter', 'true') }}
-
-                then first_value(flow_id) over (partition by person_id, flow_partition order by occurred_at asc rows between unbounded preceding and current row)
-                else null end) as last_touch_flow_id
+            when {{ dbt_utils.datediff('last_flow_touch_at', 'occurred_at', 'day') }} <= (
+                case when lower(last_flow_touch_event_type) like '%sms%' then {{ var('klaviyo__sms_attribution_lookback') }}
+                    else {{ var('klaviyo__email_attribution_lookback') }} end
+            )
+                then first_value(flow_id) over (
+                    partition by person_id, flow_partition order by occurred_at asc rows between unbounded preceding and current row)
+            else null end) as last_touch_flow_id
 
     from last_touches 
 )
