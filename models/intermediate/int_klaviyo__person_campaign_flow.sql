54dc6123489d695da733ed9b0947968357718661
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-04-28 12:02:07 -0700

    revenue metrics

--- models/intermediate/int_klaviyo__person_campaign_flow.sql
+++ models/intermediate/int_klaviyo__person_campaign_flow.sql
@@ -9,8 +9,16 @@ with events as (
 {% endset %}
 {% set conversion_metric_results = run_query(conversion_metrics_query) %}
 
+-- selects all distinct event types with some sort of revenue value attached
+{% set revenue_metrics_query %}
+    select distinct type from {{ ref('klaviyo__events') }} 
+    where {{ var('klaviyo__pivot_conversion_filter') }} and numeric_value is not null
+{% endset %}
+{% set revenue_metric_results = run_query(revenue_metrics_query) %}
+
 {% if execute %}
     {% set conversion_metrics = conversion_metric_results.columns[0].values() %}
+    {% set revenue_metrics = revenue_metric_results.columns[0].values() %}
 {% else %} -- use default klaviyo + API events
     {% set conversion_metrics = [
             'Active on Site',
@@ -34,6 +42,8 @@ with events as (
             'Unsubscribed from SMS',
             'Failed to Deliver SMS'
         ] %}
+
+    {% set revenue_metrics = ['Placed Order', 'Refunded Order'] %}
 {% endif %}
 
 pivot_out_events as (
@@ -45,10 +55,16 @@ pivot_out_events as (
         campaign_name,
         flow_name,
         variation_id
+        
+    -- look at revenue
+    {% for rm in revenue_metrics %}
+    , sum(case when lower(type) = '{{ rm | lower }}' then numeric_value else 0 end) as {{ 'sum_revenue_' ~ rm | replace(' ', '_') | lower }}
+    {% endfor %}
 
+    -- look at revenue associated stuff
     {% for cm in conversion_metrics %}
-    , sum(case when lower(type) = '{{ cm | lower }}' then 1 else 0 end) as {{ 'count_' ~ cm | replace(' ', '_') | lower }}
-    , sum(case when lower(type) = '{{ cm | lower }}' then numeric_value else 0 end) as {{ 'sum_value_' ~ cm | replace(' ', '_') | lower }}
+    , sum(case when lower(type) = '{{ cm | lower }}' then 1 else 0 end) as {{ 'count_' ~ cm | replace(' ', '_') | replace('(', '') | replace(')', '') | lower }}
+    -- can perhaps create a variable to add up any other columns...
     {% endfor %}
 
     from events
