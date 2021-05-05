2cd8946e24490ba5c758eaf9b5680eef980d15cf
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-05-05 15:49:49 -0700

    person enahnced

---
+++ models/klaviyo__persons.sql
@@ -0,0 +1,24 @@
+with person as (
+
+    select *
+    from {{ var('person') }}
+),
+
+person_metrics as (
+
+    select *
+    from {{ ref('int_klaviyo__person_metrics') }}
+),
+
+person_join as (
+
+    select
+        person.*,
+        {{ dbt_utils.star(from=ref('int_klaviyo__person_metrics'), except=['person_id'] | upper if target.type == 'snowflake' ) }}
+
+    from person
+    left join person_metrics using(person_id)
+)
+
+select *
+from person_join
