166acedf836225cd8a2b01c8d58a33aa973195bc
fivetran-reneeli <renee.li@fivetran.com>
2022-03-21 17:10:04 -0500

    try cast macro

---
+++ macros/try_cast.sql
@@ -0,0 +1,40 @@
+{% macro try_cast(field, type) %}
+    {{ return(adapter.dispatch('try_cast') (field, type)) }}
+{% endmacro %}
+
+{% macro default__try_cast(field, type) %}
+    cast({{field}} as {{type}})
+{% endmacro %}
+
+{% macro postgres__try_cast(field, type) %}
+    CREATE OR REPLACE FUNCTION try_cast(_in text, INOUT _out ANYELEMENT)
+        LANGUAGE plpgsql AS
+    $func$
+BEGIN
+   EXECUTE format('SELECT %L::%s', $1, pg_typeof(_out))
+   INTO  _out;
+EXCEPTION WHEN others THEN
+   -- do nothing: _out already carries default
+END
+$func$
+{% endmacro %}
+
+
+{% macro bigquery__try_cast(field, type) %}
+    safe_cast({{field}} as {{type}})
+{% endmacro %}
+
+
+{% macro redshift__try_cast(field, type) %}
+    try_cast({{field}} as {{type}})
+{% endmacro %}
+
+
+{% macro snowflake__try_cast(field, type) %}
+    try_cast({{field}} as {{type}})
+{% endmacro %}
+
+
+{% macro spark__try_cast(field, type) %}
+    try_cast({{field}} as {{type}})
+{% endmacro %}
